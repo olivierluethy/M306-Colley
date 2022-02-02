dd22a4ed2d8f922d0d3cdb6bcf21112bdc659120
Oli7000 <olivier.luethy@gmx.net>
2022-02-02 09:45:47 +0100

    CSS Link geändert so das er stimmt

--- app/Views/welcome.view.php
+++ app/Views/welcome.view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Meine Seite</title>
-    <link rel="stylesheet" href="public/css/app.css">
+    <link rel="stylesheet" href="../public/css/app.css">
 </head>
 <body>
 
