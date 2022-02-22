50a95d3c5d36fcb6f445098a6449382f4832d701
Oli7000 <olivier.luethy@gmx.net>
2022-02-22 10:23:31 +0100

    Neue Ansicht nicht perfekt

--- app/Views/loginRegister.view.php
+++ app/Views/loginRegister.view.php
@@ -23,40 +23,49 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
 </head>
 <body>
     <h1>Colley</h1>
-    <div class="area">
-        <div class="login">
-            <h2>Login</h2>
-            <p>Willkommen zurück bei Colley.</p>
-            <p>Bitte melden Sie sich an.</p>
-            <form id="formLogin" action="login" method="POST">
-                <label for="email">E-mail:</label>
-                <input type="email" id="email_login" name="email_login">
-                <p id="email_login_error"></p>
-                <label for="passwort">Passwort:</label>
-                <input type="password" id="passwort_login" name="passwort_login">
-                <p id="passwort_login_error"></p>
-                <button class="login" type="submit">Login</button>
-            </form>
-        </div>
-        <div class="divider"></div>
-        <div class="newAccount">
-            <h2>neuer Account</h2>
-            <p>Willkommen bei Colley.</p>
-            <p>Noch kein Konto? Kein Problem. Bitte erstellen Sie in Konto.</p>
-            <form id="formRegister" action="register" method="POST">
-                <label for="email">E-mail:</label>
-                <input type="email" id="email_register" name="email_register">
-                <p id="email_register_error"></p>
-                <label for="passwort">Passwort:</label>
-                <input type="password" id="passwort_register" name="passwort_register">
-                <p id="passwort_register_error"></p>
-                <label for="passwort_verify">Passwort:</label>
-                <input type="password" id="passwort_verify" name="passwort_verify">
-                <p id="passwortverify_register_error"></p>
-                <button class="newAccount" type="submit">Sign Up</button>
-            </form>
-        </div>
-    </div>
+    <table>
+        <tr>
+            <th>
+                <div class="login">
+                    <h2>Login</h2>
+                    <p>Willkommen zurück bei Colley.</p>
+                    <p>Bitte melden Sie sich an.</p>
+                    <form id="formLogin" action="login" method="POST">
+                        <label for="email">E-mail:</label>
+                        <input type="email" id="email_login" name="email_login">
+                        <p id="email_login_error"></p>
+                        <label for="passwort">Passwort:</label>
+                        <input type="password" id="passwort_login" name="passwort_login">
+                        <p id="passwort_login_error"></p>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </th>
+            <th>
+                <div class="divider"></div>
+            </th>
+            <th>
+                <div class="newAccount">
+                    <h2>neuer Account</h2>
+                        <p>Willkommen bei Colley.</p>
+                        <p>Noch kein Konto? Kein Problem. Bitte erstellen Sie in Konto.</p>
+                        <form id="formRegister" action="register" method="POST">
+                            <label for="email">E-mail:</label>
+                            <input type="email" id="email_register" name="email_register">
+                            <p id="email_register_error"></p>
+                            <label for="passwort">Passwort:</label>
+                            <input type="password" id="passwort_register" name="passwort_register">
+                            <p id="passwort_register_error"></p>
+                            <label for="passwort_verify">Passwort:</label>
+                            <input type="password" id="passwort_verify" name="passwort_verify">
+                            <p id="passwortverify_register_error"></p>
+                            <button type="submit">Sign Up</button>
+                        </form>
+                </div>
+            </th>
+        </tr>
+    </table>
+
     <script src="public/js/clientSideValidationRegister.js"></script>
     <script src="public/js/clientSideValidationLogin.js"></script>
 </body>
--- app/Views/sideNav.view.php
+++ app/Views/sideNav.view.php
@@ -3,17 +3,17 @@
     <h4>Buchhaltungssoftware</h4>
     <hr>
     <a class="closebtn" onclick="closeNav()">&times;</a>
-    <a href="home"><img src="assets/home.png" alt="Startseite"> Startseite</a>
-    <a href="#"><img src="assets/account.jpg" alt="Neues Konto"> Neues Konto</a>
-    <a href="bilanz"><img src="assets/balance.png" alt="Bilanz"> Bilanz</a>
-    <a href="#"><img src="assets/chart-line.png" alt="Erfolgsrechnung"> Erfolgsrechnung</a>
-    <a href="#"><img src="assets/jahresabschluss.jpg" alt="Jahresabschluss"> Jahresabschluss</a>
-    <a href="#"><img src="assets/piggy-bank.png" alt="Kalkulation"> Kalkulation</a>
+    <a href="home"><img src="assets/home.png" alt="Startseite">&nbsp Startseite</a>
+    <a href="neues-konto"><img src="assets/account.jpg" alt="Neues Konto">&nbsp Neues Konto</a>
+    <a href="bilanz"><img src="assets/balance.png" alt="Bilanz">&nbsp Bilanz</a>
+    <a href="#"><img src="assets/chart-line.png" alt="Erfolgsrechnung">&nbsp Erfolgsrechnung</a>
+    <a href="#"><img src="assets/jahresabschluss.jpg" alt="Jahresabschluss">&nbsp Jahresabschluss</a>
+    <a href="#"><img src="assets/piggy-bank.png" alt="Kalkulation">&nbsp Kalkulation</a>
     <hr>
     <div class="eingeloggt">
     <?php
     echo "<p>Angemeldet mit:<br>" . $_SESSION['email'] ."</p>";
     ?>
     </div>
-    <button href="logout">Logout</button>
+    <button onclick="logout()">Logout</button>
 </div>
