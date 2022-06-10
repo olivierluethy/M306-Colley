08e3272640262005acd9a17f1144ed366bb8544f
Demelsa <demelsa_speiser@sluz.ch>
2022-06-10 11:04:25 +0200

    update

--- Demelsa/app/Controllers/inc/bilanz/kontoErstellen.inc.php
+++ Demelsa/app/Controllers/inc/bilanz/kontoErstellen.inc.php
@@ -3,18 +3,14 @@
 if($_SERVER['REQUEST_METHOD'] == 'POST'):
     if((isset($_POST['kontoNr'])) && (isset($_POST['kontoName'])) && (isset($_POST['kontoZweck']))):
         if(!empty($_POST['kontoNr']) && !empty($_POST['kontoName']) && !empty($_POST['kontoZweck'])):
-            if(in_array($_POST['kontoNr'], range('1000', '1999'))):
-                $_POST['untergruppe'] = 'Aktive';
-            endif;
             $createKonto = new ColleyKonto(
                 htmlspecialchars($_POST['kontoNr']),
                 htmlspecialchars($_POST['kontoName']),
                 htmlspecialchars($_POST['kontoName']),
-                htmlspecialchars($_POST['untergruppe']),
                 false
             );
             $createKonto->create();
-                else:
+        else:
             $error = 'Es gab ein Problem. Bitte füllen Sie alle Felder aus. Vielen Dank.';
         endif;
     else:
